ed4d7d3c3c94cb76fa33cd1fc23ac4e1c81ad8c2
mfmiller7 <mfmiller@bu.edu>
2024-04-11 16:22:54 -0400

    cleaned up backend

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,6 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// require("dotenv").config(); // If using dotenv for environment variables
-// const routes = require("./routes");
-// const beer = require("./beer");
-// const cors = require("cors");
-//
-// const app = express();
-// app.use(cors());
-// const port = process.env.PORT || 3000;
-//
-// mongoose
-//     .connect(process.env.MONGODB_URI)
-//     .then(() => console.log("Connected to MongoDB"))
-//     .catch((err) => console.error("Could not connect to MongoDB:", err));
-//
-// app.use(express.json());
-// //app.use("/", routes);
-// app.use("/", beer);
-//
-// app.listen(port, () => {
-//     console.log(`Server running at http://localhost:${port}`);
-// });
-
 const { MongoClient } = require('mongodb');
 const express = require('express');
 const beer = require("./beer");
-
 const app = express();
 const port = 3000;
 async function main(){
@@ -37,16 +12,12 @@ async function main(){
         console.error(e);
     }
 }
-
 main().catch(console.error);
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
 app.use("/", beer);
 app.get("/", (req, res) => {
     res.send("use /beers/:country to get beer list for a country");
