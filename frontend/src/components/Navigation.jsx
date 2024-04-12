200851ab329d23848696d9cd18c47ad7e8620c89
mfmiller7 <mfmiller@bu.edu>
2024-04-11 21:48:13 -0400

    fixed nav logic, still re-render bug

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -29,27 +29,51 @@ const StyledNav=styled.nav`
         color: black;
     }
 `
+
 export default function Navigation() {
+    const countries = [
+        'belgium', 'czech', 'denmark', 'finland', 'ireland',
+        'italy', 'luxembourg', 'malta', 'norway', 'poland',
+        'portugal', 'spain', 'sweden', 'switzerland'
+    ];
+
     return (
         <>
             <StyledNav>
                 <ul>
-                    <li><NavLink to={'/belgium'}>Belgium</NavLink></li>
-                    <li><NavLink to={'/czech'}>Czech</NavLink></li>
-                    <li><NavLink to={'/denmark'}>Denmark</NavLink></li>
-                    <li><NavLink to={'/finland'}>Finland</NavLink></li>
-                    <li><NavLink to={'/ireland'}>Ireland</NavLink></li>
-                    <li><NavLink to={'/italy'}>Italy</NavLink></li>
-                    <li><NavLink to={'/luxembourg'}>Luxembourg</NavLink></li>
-                    <li><NavLink to={'/malta'}>Malta</NavLink></li>
-                    <li><NavLink to={'/norway'}>Norway</NavLink></li>
-                    <li><NavLink to={'/poland'}>Poland</NavLink></li>
-                    <li><NavLink to={'/portugal'}>Portugal</NavLink></li>
-                    <li><NavLink to={'/spain'}>Spain</NavLink></li>
-                    <li><NavLink to={'/sweden'}>Sweden</NavLink></li>
-                    <li><NavLink to={'/switzerland'}>Switzerland</NavLink></li>
+                    {countries.map(country => (
+                        <li key={country}>
+                            <NavLink to={`/${country}`}>{country.toUpperCase()}</NavLink>
+                        </li>
+                    ))}
                 </ul>
             </StyledNav>
         </>
     )
 }
+
+
+// export default function Navigation() {
+//     return (
+//         <>
+//             <StyledNav>
+//                 <ul>
+//                     <li><NavLink to={'/belgium'}>Belgium</NavLink></li>
+//                     <li><NavLink to={'/czech'}>Czech</NavLink></li>
+//                     <li><NavLink to={'/denmark'}>Denmark</NavLink></li>
+//                     <li><NavLink to={'/finland'}>Finland</NavLink></li>
+//                     <li><NavLink to={'/ireland'}>Ireland</NavLink></li>
+//                     <li><NavLink to={'/italy'}>Italy</NavLink></li>
+//                     <li><NavLink to={'/luxembourg'}>Luxembourg</NavLink></li>
+//                     <li><NavLink to={'/malta'}>Malta</NavLink></li>
+//                     <li><NavLink to={'/norway'}>Norway</NavLink></li>
+//                     <li><NavLink to={'/poland'}>Poland</NavLink></li>
+//                     <li><NavLink to={'/portugal'}>Portugal</NavLink></li>
+//                     <li><NavLink to={'/spain'}>Spain</NavLink></li>
+//                     <li><NavLink to={'/sweden'}>Sweden</NavLink></li>
+//                     <li><NavLink to={'/switzerland'}>Switzerland</NavLink></li>
+//                 </ul>
+//             </StyledNav>
+//         </>
+//     )
+// }
